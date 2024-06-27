f9209633591cf67d72b8f4af9a81fc76c2a9838a
Colby Jensen <endergdawg@gmail.com>
2024-06-27 11:08:31 -0600

    add calculator styling and logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import './App.css'
+import Calculator from './Calculator'
 
 function App() {
 
   return (
     <>
+      <header>
+        <h1>Simple Calculator</h1>
+      </header>
+      <div>
+        <Calculator />
+      </div>
+      <footer>
+        <p>created by cob salad</p>
+      </footer>
     </>
   )
 }
